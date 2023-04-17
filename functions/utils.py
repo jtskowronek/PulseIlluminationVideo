5a59f46a53a513312d34a116de6002f23fc827cf
FOGuzman <felipe.guzman.v@mail.pucv.cl>
2023-04-17 15:16:10 -0700

    update

--- functions/utils.py
+++ functions/utils.py
@@ -15,3 +15,13 @@ def meas2tensor(params):
      input = input.cuda().float()
      return input,im
 
+def code2tensor(params):
+
+    code = np.array(params.code)
+    code = torch.from_numpy(code)
+
+    mask = torch.ones(size=(params.size[0],params.size[1],params.frames))
+    mask = mask*code
+    mask = torch.permute(mask,(2,0,1))
+    mask = torch.unsqueeze(mask,0).cuda().float()
+    return(mask)
