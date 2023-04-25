a86c933e69818463dd86ed7fa9efcdc64bd52ae1
FOGuzman <felipe.guzman.v@mail.pucv.cl>
2023-04-25 14:44:07 -0700

    add sci3d

--- functions/GetNetwork.py
+++ functions/GetNetwork.py
@@ -1,6 +1,7 @@
 from models.skip import skip
 from models.revsci import re_3dcnn
 from models.frameNet import FrameNet
+from models.HQS_temporal_3DConv_RFMb_HSA import HQSNet
 
 
 def getnetwork(params):
@@ -19,7 +20,15 @@ def getnetwork(params):
                upsample_mode='nearest', filter_skip_size=1,
                need_sigmoid=True, need_bias=True, act_fun='LeakyReLU')
    
-       
+
+    if params.network == 'SCI3D': 
+        cnnpar = argparse.ArgumentParser(description='Setting, compressive rate, size, and mode')
+        cnnpar.add_argument('--layer_num', type=int, default=10, help='phase number of HQS-Net')
+        cnnpar.add_argument('--n_channels', type=int, default=1, help='1 for gray, 3 for color')
+        cnnpar.add_argument('--temporal_patch', type=int, default=3,
+                    help='the number of frames where temporal step takes as input')
+        model = HQSNet(cnnpar)
+
     if params.network == 'FrameNet':
        net = FrameNet()   
 
